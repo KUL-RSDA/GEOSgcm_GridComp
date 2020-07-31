3c103d7aa5e3c547ffb866fd2521ecb7b34cfcac
Santha Akella <santha.akella@nasa.gov>
2020-07-31 13:24:46 -0400

    rename pseudoCode.F90 to README.md with the intent of rewriting, update CMakeLists.txt

---
+++ GEOSdataatm_GridComp/data_water.F90
@@ -0,0 +1,16 @@
+!  $Id$
+
+#include "MAPL_Generic.h"
+
+!=============================================================================
+!BOP
+
+! !MODULE: data_waterMod -- get ``atmospheric" variables for driving open ocean
+
+! !INTERFACE:
+
+module data_water_mod
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+end module data_water_mod
